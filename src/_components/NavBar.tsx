3d6c6e85633277881ee2369ec100296d9139de74
mariakrishna <mkadonai@gmail.com>
2022-05-16 21:39:51 -0300

    navbar component included. hero features modified

--- src/_components/NavBar.tsx
+++ src/_components/NavBar.tsx
@@ -1,43 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { faSearch, faCaretDown } from "@fortawesome/free-solid-svg-icons";
-import logo from '../_assets/imagiflix.png';
-import avatar from '../_assets/avatar.jpg';
+import logo from "../_assets/imagiflix.png";
+import avatar from "../_assets/avatar.jpg";
 
-const NavBar = () => (
-    <nav className="absolute top-0 left -0 grid grid-cols-2 w-full p-8">
+import "./NavBar.css";
 
-        <div className="justify-self-start grid grid-cols-2 gap-4 items-center">
-            <h1 className="hidden">Imagiflix</h1>
-          <img src={logo} alt='Imagiflix' />  
+const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
 
-           <ul className="grid grid-flow-col gap-4">
-            <li className="font-bold">Início</li>
-            <li><a href="#series">Séries</a></li>
-            <li><a href="#movies">Filmes</a></li>
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <nav className="navbar absolute top-0 left -0 grid grid-cols-2 w-full p-8">
+      <div className="justify-self-start grid grid-cols-2 gap-4 items-center">
+        <h1 className="hidden">Imagiflix</h1>
+        <img src={logo} alt="Imagiflix" />
+
+        <ul className="grid grid-flow-col gap-4">
+          <li className="font-bold">Início</li>
+          <li>
+            <a href="#series">Séries</a>
+          </li>
+          <li>
+            <a href="#movies">Filmes</a>
+          </li>
         </ul>
-        </div>
-        
-       
+      </div>
 
-        <div className="justify-self-end flex justify-items-end items-center">
-            <form className="relative w-64">
-                <input className='w-full bg-black border border-white rounded py-1 px-3 transition-all duration-300 ease-in-out opacity-0 hover:opacity-100 focus:opacity-100' type='text' placeholder="Títulos, gente e gêneros" />
-                <button className="absolute right-2 py-1"><FontAwesomeIcon icon={faSearch} /></button>
-            </form>
-            <div className="flex ml-4">
-                <img src={avatar} alt='Foto do Usuário' />
-                <button>
-                    <FontAwesomeIcon className='ml-2' icon={faCaretDown} />
-                </button>
-                <ul className="hidden">
-                    <li><a href="#account">Minha Conta</a></li>
-                    <li><a href="#logout">Sair</a></li>
-                </ul>
-            </div>
+      <div className="justify-self-end flex justify-items-end items-center">
+        <form className="relative w-64">
+          <input
+            className="w-full bg-black border border-white rounded py-1 px-3 transition-all duration-300 ease-in-out opacity-0 hover:opacity-100 focus:opacity-100"
+            type="text"
+            placeholder="Títulos, gente e gêneros"
+          />
+          <button
+            className="absolute right-2 py-1"
+            onClick={(e) => e.preventDefault()}
+          >
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </form>
+        <div className="relative flex ml-4">
+          <img src={avatar} alt="Foto do Usuário" onClick={handleClick} />
+          <button onClick={handleClick}>
+            <FontAwesomeIcon className="ml-2" icon={faCaretDown} />
+          </button>
+          <ul
+            className={`absolute mt-10 top-0 right-0 w-32 bg-black rounded p-4 transition-all duration-300 
+            ease-in-out ${!showMenu && "invisible opacity-0"} `}
+          >
+            <li>
+              <a href="#account" onClick={handleClick}>
+                Minha Conta
+              </a>
+            </li>
+            <li>
+              <a href="#logout" onClick={handleClick}>
+                Sair
+              </a>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
-);
+  );
+};
 
 export default NavBar;
--- src/_components/hero.tsx
+++ src/_components/hero.tsx
@@ -1,10 +1,47 @@
 import React from "react";
-import placeholder from '../_assets/bg_avengers.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPlus } from "@fortawesome/free-solid-svg-icons";
 
-const Hero = () => (
-    <header className="min-h-screen">
-        <img className="object-cover object-center h-full w-full" src={placeholder} alt='Filme em destaque' />
+import placeholder from "../_assets/bg_avengers.jpg";
+
+const Hero = ({ title = "Avengers Endgame", score = 10 }) => {
+  const getBorderColor = () => {
+    if (score >= 7) {
+      return "border-green-400";
+    } else if (score > 4 && score < 7) {
+      return "border-yellow-400";
+    } else {
+      return "border-red-400";
+    }
+  };
+
+  return (
+    <header className="relative min-h-screen">
+      <img
+        className="object-cover object-center h-full w-full"
+        src={placeholder}
+        alt="Filme em destaque"
+      />
+      <article className="absolute bottom-0 mb-64 px-8">
+        <p className="text-2xl">Assista agora:</p>
+        <h2 className="text-5xl font-black pb-2">{title}</h2>
+        <p className="text-base">
+          Nota{" "}
+          <span
+            className={`inline-block mx-2 py-2 px-3 border border-4 bg-black bg-opacity-75 rounded-full ${getBorderColor()}`}
+          >
+            {score}
+          </span>
+        </p>
+        <button className="text-base py-2 px-8 mr-2 mt-8 rounded bg-black bg-opacity-50 transition-all duration-300 ease-in-out hover:bg-white hover:text-black hover:bg-opacity-75">
+          <FontAwesomeIcon className="mr-2" icon={faPlay} /> Assistir
+        </button>
+        <button className="text-base py-2 px-8 mr-2 mt-8 rounded bg-black bg-opacity-50 transition-all duration-300 ease-in-out hover:bg-white hover:text-black hover:bg-opacity-75">
+          <FontAwesomeIcon className="mr-2" icon={faPlus} /> Minha Lista
+        </button>
+      </article>
     </header>
-);
+  );
+};
 
 export default Hero;
